c9dbf42d1b679ecfe1873c66f9fae2a134383924
Moyo Dawodu <moyodawodu@gmail.com>
2024-02-25 11:19:28 -0700

    Added some javadocs and refined user Constructor.

--- app/src/main/java/com/example/scandroid/User.java
+++ app/src/main/java/com/example/scandroid/User.java
@@ -1,30 +1,66 @@
 package com.example.scandroid;
 
 import android.media.Image;
+import android.provider.Settings;
 
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.UUID;
+import java.util.Random;
 
+/**
+ * Represents a User in the scANDROID app for CMPUT 301 at UofA W24. <br>
+ * This class may represent a user with different permission levels - such as an Admin or an organizer or attendee.<br>
+ * Users are stored via a UserDB object.
+ * @author Jordan Beaubien & Moyo Dawodu
+ */
 public class User {
-    //NOTE - Comments can be moved to javadocs later
-    //userID is gotten from Settings.Secure.ANDROID_ID and exists for the purpose of identifying unique users
-    private String userID;
-    //TODO - set default value of userName to "Guest" + "randint"
     private String userName;
     private String userEmail;
     private String userPhoneNumber;
-    //TODO - character limit for userAboutMe on Front end
+    //TODO - (FRONT END) set character limit for userAboutMe.
     private String userAboutMe;
     public Image userProfileImage;
     private int totalAttended;
-    public Boolean isAdmin;
+    private String adminKey;
+    private String userID;
     private ArrayList<UUID> eventsAttending;
     private ArrayList<UUID> eventsOrganized;
 
-    public User(String userID, ArrayList<UUID> eventsAttending, ArrayList<UUID> eventsOrganized) {
-        this.userID = userID;
-        this.eventsAttending = eventsAttending;
-        this.eventsOrganized = eventsOrganized;
+    /* ----------- *
+     * CONSTRUCTOR *
+     * ----------- */
+
+    /**
+     * Sole constructor for the <code>User</code> object, specifying user attributes.
+     *
+     * @param userName The name of the User object you want to instantiate. This is set to a random guest name if none is provided.
+     * @param userPhoneNumber The phone number of a user object. May be null.
+     * @param userAboutMe The about-me section of a user's profile. May be null.
+     * @param userProfileImage The profile image of a user object. This is randomized if none is provided.
+     * @param userEmail The email address of a user object. May be null.
+     */
+    public User(String userName, String userPhoneNumber, String userAboutMe, Image userProfileImage, String userEmail) {
+        this.userID = Settings.Secure.ANDROID_ID;
+        this.eventsAttending = new ArrayList<UUID>();
+        this.eventsOrganized = new ArrayList<UUID>();
+        //set the default value of a user's name to a Guest name if no name is provided.
+        if(userName == null){
+            Random random_num = new Random();
+            int randID = random_num.nextInt(10000);
+            this.userName = "Guest" + randID;
+        }
+        else{
+            this.userName = userName;
+        }
+        this.userAboutMe = userAboutMe;
+        this.userProfileImage = userProfileImage;
+        this.userPhoneNumber = userPhoneNumber;
+        this.userEmail = userEmail;
+    }
+
+    public boolean isAdmin(String userAdminKey){
+        return Objects.equals(userAdminKey, adminKey);
     }
 
     //TODO - getters and setters
@@ -70,8 +106,12 @@ public class User {
         this.totalAttended = totalAttended;
     }
 
+
     //TODO - notifiedBy setup
+
+    //contains a list of eventIDs
+    public ArrayList<UUID> notifiedBy;
     //TODO - scanQRCode and createEvent
     //TODO - UserDB
-    //TODO - (low priority) javadocs
+    //TODO - javadocs
 }
