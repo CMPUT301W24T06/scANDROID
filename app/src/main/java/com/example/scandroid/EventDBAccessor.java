eda2380a699c4e1bea58dbcbf5b480bff8120586
jordanbeaubien <jordanbeaubien@me.com>
2024-02-28 20:01:38 -0700

    add getters for DBAccessors

--- app/src/main/java/com/example/scandroid/EventDBAccessor.java
+++ app/src/main/java/com/example/scandroid/EventDBAccessor.java
@@ -4,9 +4,12 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 /**
@@ -58,6 +61,30 @@ public class EventDBAccessor {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Log.w("Firestore", "Error writing Event document", e);
+                    }
+                });
+    }
+
+    /**
+     * Get Event stored in Firestore Database
+     * @param eventID Unique identifier for Event
+     */
+    public void getEvent(String eventID) {
+        // Source: https://firebase.google.com/docs/firestore/query-data/get-data
+        EventRef.document(eventID).get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                Log.d("Firestore", "DocumentSnapshot data: " + document.getData());
+                            } else {
+                                Log.d("Firestore", "No such document");
+                            }
+                        } else {
+                            Log.d("Firestore", "get failed with ", task.getException());
+                        }
                     }
                 });
     }
