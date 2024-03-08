5b3411f05891a748a4956c9aa5620517fa5b1de9
ag1rlisagun <aaliyahwusu@gmail.com>
2024-03-07 21:17:14 -0700

    created EventCheckInFragment,  linked to QRScanner

--- app/src/main/java/com/example/scandroid/EventInfoActivity.java
+++ app/src/main/java/com/example/scandroid/EventInfoActivity.java
@@ -1,14 +1,16 @@
 package com.example.scandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class EventInfoActivity extends AppCompatActivity {
+    Event event;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_info);
+        event = (Event)getIntent().getSerializableExtra("event");
     }
 }
