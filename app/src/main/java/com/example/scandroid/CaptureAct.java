99df9d9b4df92093a07262f30b440aa326688986
jaderpana <jaderachellepana@gmail.com>
2024-03-08 02:10:14 -0700

    DeviceIDRetriever JavaDoc

--- app/src/main/java/com/example/scandroid/CaptureAct.java
+++ app/src/main/java/com/example/scandroid/CaptureAct.java
@@ -2,5 +2,11 @@ package com.example.scandroid;
 
 import com.journeyapps.barcodescanner.CaptureActivity;
 
+/**
+ * CaptureAct is the capture activity for scanning barcodes within the application.
+ * This class is used to customize the behavior and appearance of the barcode scanner
+ * found in QRScannerActivity.
+ * It extends CaptureActivity provided by the journeyapps/barcodescanner library.
+ */
 public class CaptureAct extends CaptureActivity {
 }
