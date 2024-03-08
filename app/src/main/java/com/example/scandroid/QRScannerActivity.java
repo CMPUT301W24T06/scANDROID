fe77f3234a91ee41e32ccc7541e2c7fca974f12f
ag1rlisagun <aaliyahwusu@gmail.com>
2024-03-07 23:35:44 -0700

    fixed qr share, linked qr scanner to new EventCheckInFragment

--- app/src/main/java/com/example/scandroid/QRScannerActivity.java
+++ app/src/main/java/com/example/scandroid/QRScannerActivity.java
@@ -1,19 +1,18 @@
 package com.example.scandroid;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Button;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-import com.journeyapps.barcodescanner.CaptureActivity;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
@@ -57,25 +56,36 @@ public class QRScannerActivity extends AppCompatActivity {
 
     private void scanCode() {
         ScanOptions options = new ScanOptions();
-        options.setPrompt("Please center barcode in box to scan. Volume up for flash!");
+        options.setPrompt("Temporary Message");
         options.setBeepEnabled(true);
         options.setOrientationLocked(true);
         options.setCaptureActivity(CaptureAct.class);
         barLauncher.launch(options);
     }
 
-    // replace this with our actual content design in the future
+    // replace this with our actual content class
     ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result ->{
         if(result.getContents() != null){
-            AlertDialog.Builder builder = new AlertDialog.Builder(QRScannerActivity.this);
-            builder.setTitle("Content");
-            builder.setMessage(result.getContents());
-            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                }
-            }).show();
+            // source: https://stackoverflow.com/a/15392591
+            DialogFragment checkInPrompt = new EventCheckInFragment();
+            String eventID = result.getContents();
+            Bundle bundle = new Bundle();
+            bundle.putString("eventID", eventID);
+            checkInPrompt.setArguments(bundle);
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.add(android.R.id.content, checkInPrompt);
+            transaction.commit();
+
+//            AlertDialog.Builder builder = new AlertDialog.Builder(QRScannerActivity.this);
+//            builder.setTitle("Content");
+//            builder.setMessage(result.getContents());
+//            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    dialog.dismiss();
+//                }
+//            }).show();
         }
     });
 }
